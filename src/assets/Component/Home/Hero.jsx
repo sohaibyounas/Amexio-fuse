e4cb0070c4a70b3c2f263990bb8cb8f7dd885da2
sohaibyounas <sohaibyounas24@gmail.com>
2025-05-14 12:10:32 +0500

    update

--- src/assets/Component/Home/Hero.jsx
+++ src/assets/Component/Home/Hero.jsx
@@ -5,44 +5,41 @@ const Hero = () => {
   return (
     <div className="relative overflow-hidden min-h-[300px] lg:bg-none">
       <div
-        className="absolute bg-no-repeat bg-contain bg-left-bottom opacity-40 block md:hidden"
+        className="absolute bg-no-repeat bg-contain bg-left-bottom opacity-50 block lg:hidden"
         style={{ backgroundImage: `url(${hero})` }}
       ></div>
 
       {/* hero section */}
       <div className="mt-10 flex justify-between items-center w-full px-2">
         <div className="cursor-pointer">
-          <h1 className="text-white ml-2 sm:ml-3 md:ml-6 text-[40px] sm:text-[65px] md:text-[65px] font-bold max-w-full">
+          <h1 className="text-white ml-6 text-[65px] font-bold max-w-[650px]">
             Connecting the dots, from
-            <span className="text-[40px] sm:text-[65px] md:text-[65px] text-[#FFC256] font-bold px-3">
+            <span className="text-[65px] text-[#FFC256] font-bold px-3">
               content
             </span>
             to experience.
           </h1>
-          <p className="text-white text-[25px] sm:text-[28px] font-medium pl-2 sm:pl-8 md:pl-8 pt-10 pr-2 sm:pr-0 max-w-[650px]">
+          <p className="text-white text-[28px] font-medium pl-8 pt-10 max-w-[650px]">
             AmeXio Fuse helps you to optimize your content operations, making
             your
             <span className="text-[#FFC256] px-2 ">customer experiences</span>
             smarter, faster, and more affordable.
           </p>
         </div>
-
+        
         {/* image */}
         <div className="hidden lg:block">
           <img
             src={hero}
             alt="heroimage"
-            className="w-[70rem] h-auto object-contain absolute -top-20 left-125"
+            className="w-[70rem] h-auto object-contain filter brightness-0 invert absolute -top-20 left-135 text-blue-700"
           />
         </div>
       </div>
 
       {/* SubText */}
-      <div
-        className="group flex items-center rounded-[25px] w-fit pr-1 sm:pr-2 ml-[35px] mt-10 mb-15 
-        bg-[#FFC256] p-3 cursor-pointer"
-      >
-        <div className="pl-[5px] hidden sm:block">
+      <div className="group flex items-center rounded-[25px] w-[400px] max-w-xl ml-[35px] mt-10 mb-15 bg-[#FFC256] p-3 cursor-pointer ">
+        <div className="pl-[5px]">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
@@ -58,7 +55,7 @@ const Hero = () => {
             />
           </svg>
         </div>
-        <p className="font-medium pl-[5px] sm:pl-[10px] pr-4 sm:pr-0 py-1 sm:py-0 transition-transform duration-300 ease-in-out group-hover:translate-x-1">
+        <p className="font-medium pl-[10px] transition-transform duration-300 ease-in-out group-hover:translate-x-1">
           Talk to us about your content challenge
         </p>
       </div>
