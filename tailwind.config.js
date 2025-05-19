2da5228fc3b69da9449ec01d26ef594e90bb4916
sohaibyounas <sohaibyounas24@gmail.com>
2025-05-20 00:14:29 +0500

    new

--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  content: [
-    "./src/**/*.{html,js}",
-    "./node_modules/tw-elements/js/**/*.js"
-  ],
-  plugins: [require("tw-elements/plugin.cjs")],
-  darkMode: "class"
-};
